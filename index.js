498bf486ccab261ccb127b9b18cd792981d64383
Manh Cuong <40521173+cuongpiger@users.noreply.github.com>
2020-07-12 15:21:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 document.querySelector('.btn-convert').addEventListener('click', function() {
     let trans = document.querySelector('.txa-origin').value;
+    trans = trans.split('\n\n').join('^');
     trans = trans.split('\n').join(' ');
+    trans = trans.split('^').join('\n\n');
+    trans = trans.split('’').join(`'`);
+    trans = trans.split('”').join(`"`);
+    trans = trans.split('“').join(`"`);
 
     document.querySelector('.txa-result').value = trans;
 });
