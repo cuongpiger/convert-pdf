fb90d8d255387ad2b13973c502d81f923c29af9e
Manh Cuong <40521173+cuongpiger@users.noreply.github.com>
2020-07-15 22:46:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,4 +8,22 @@ document.querySelector('.btn-convert').addEventListener('click', function() {
     trans = trans.split('“').join(`"`);
 
     document.querySelector('.txa-result').value = trans;
+    document.querySelector('.txa-origin').value = "";
+});
+
+$(document).ready(function() {
+    $('#textareaId').keydown(function (e) {
+        if (e.ctrlKey && e.keyCode == 13) {
+            let trans = document.querySelector('.txa-origin').value;
+            trans = trans.split('\n\n').join('^');
+            trans = trans.split('\n').join(' ');
+            trans = trans.split('^').join('\n\n');
+            trans = trans.split('’').join(`'`);
+            trans = trans.split('”').join(`"`);
+            trans = trans.split('“').join(`"`);
+
+            document.querySelector('.txa-result').value = trans;
+            document.querySelector('.txa-origin').value = "";
+        }
+    });
 });
